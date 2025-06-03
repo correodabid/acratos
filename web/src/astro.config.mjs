3666e3ecaca1eab904d9d9557f17d69aadafa65d
correodabid <correodabid@gmail.com>
2025-06-03 15:26:52 +0200

    Fix GitHub Pages deployment

--- web/src/astro.config.mjs
+++ web/src/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://correodabid.github.io',
-  base: '/acratos',
+  base: '/acratos/',
   integrations: [tailwind()],
   output: 'static',
   build: {
